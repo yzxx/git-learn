50dfaa4bd6be69ab5572f726e8b7676329d316e3
zhangyuzhuo <zhangyuzhuo@163.com>
2016-10-20 11:41:03 +0800

    add foo.c

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern void foo();
+
 int main()
 {
 	printf("hello, world!\n");
 	printf("hello, git!\n");
 	printf("hello, zhangyuzhuo!\n");
 	printf("hello, zhanglei!\n");
+	foo();	
 	return 0;
 }
